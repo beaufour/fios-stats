36766047ca0e5de3e74e05a880fe9bd50336f945
Allan Beaufour <allan@beaufour.dk>
2019-09-05 16:37:14 -0400

    First implementation

---
+++ README.md
@@ -0,0 +1,10 @@
+Simple program to fetch stats from the Fios Quantum Gateway router.
+
+Usage:
+    > `fios-stats -p <admin password> [-i <influx_db_uri> ]`
+
+Example:
+    > `fios-stats -p secret_password -i 'http://192.168.0.12:8086/write?db=fios_data'`
+
+This fetches the stats using the admin password `secret_password` and stores the data in the influxdb at
+`http://192.168.0.12:8086/write?db=fios_data`.
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,36 @@
-extern crate hyper;
-extern crate native_tls;
-extern crate hyper_tls;
+// Fetches network stats from the Fios Quantum G1000 router
+//
+// Their admin interface fetches data using a standard JSON REST API. If there is a way to
+// authenticate that is different than how a browser does, I don't know of it. So that's what I do
+// here. And then I just call /api/network/1, parse the results, and send the data to influx db.
+//
+// The program expects the router to be found on myfiosgateway.com
+//
+// The authentication works this way:
+// 1) You call /login to get a passwordSalt.
+// 2) you take the Sha512(password + passwordSalt) to create a hash
+// 3) you call /login with {"password": hash}
+// 4) on successful login, two cookies are returned XSRF-TOKEN and Session
+// For all API calls set Session as a cookie and a header X-XSRF-TOKEN with the XSRF-TOKEN value
 
-use hyper::Client;
-use hyper::client::{HttpConnector};
-use hyper::rt::{self, Future, Stream};
-use hyper_tls::HttpsConnector;
 
+// TODO
+// * add tests
+// * add Travis, https://docs.travis-ci.com/user/languages/rust/
+// * nicer error handling. At least for: 1) unexpected data in /network/1 return, and 2) auth errors
+
+#[macro_use]
+extern crate simple_error;
+
+use clap::{App, Arg};
+use env_logger::{Env};
+use log::debug;
+use reqwest;
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use sha2::{Sha512, Digest};
+use std::collections::HashMap;
+use tokio;
 
 #[derive(Serialize, Deserialize, Debug)]
 #[allow(non_snake_case)]
@@ -24,63 +47,194 @@ struct LoginResponse {
     meshUserEnabledConfig: bool,
 }
 
-fn main() {
-    let uri = "https://myfiosgateway.com/api/login".parse().unwrap();
+#[derive(Debug, Default)]
+struct AuthInfo {
+    token: String,
+    session: u32,
+}
 
-    let fut = fetch_json(uri)
-        .map(|response| {
-            println!("response: {:#?}", response);
-            println!("salt: {}", response.passwordSalt);
-        })
-        .map_err(|e| {
-            match e {
-                FetchError::Http(e) => eprintln!("http error: {}", e),
-                FetchError::Json(e) => eprintln!("json parsing error: {}", e),
+const BASE_URI:&str = "https://myfiosgateway.com/api/";
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let matches = App::new("Fios Gateway Stats Retriever")
+        .version("0.1.0")
+        .author("Allan Beaufour <allan@beaufour.dk>")
+        .arg(Arg::with_name("password")
+             .short("p")
+             .long("password")
+             .value_name("PASSWORD")
+             .help("Password for router")
+             .required(true)
+             .takes_value(true))
+        .arg(Arg::with_name("influx_db")
+             .short("i")
+             .long("influxdb")
+             .value_name("URI")
+             .help("URI to InfluxDB including databasename")
+             .required(true)
+             .takes_value(true))
+        .get_matches();
+    let password = matches.value_of("password").unwrap();
+    let influx_db = matches.value_of("influx_db").unwrap();
+
+    let env = Env::default()
+        .filter_or("MY_LOG_LEVEL", "info")
+        .write_style_or("MY_LOG_STYLE", "always");
+    env_logger::init_from_env(env);
+
+    let client = reqwest::Client::builder()
+        // Unknown CA, and I'm not sure all devices use the same...
+        .danger_accept_invalid_certs(true)
+        .build()?;
+
+    let login_info = get_login_info(&client)?;
+    debug!("Got login info: {:#?}", login_info);
+
+    let auth_info = do_login(&client, &password, &login_info.passwordSalt)?;
+    debug!("Got auth info: {:#?}", auth_info);
+
+    let mut headers = reqwest::header::HeaderMap::new();
+    headers.insert(reqwest::header::HeaderName::from_static("x-xsrf-token"),
+                   reqwest::header::HeaderValue::from_str(&auth_info.token)?);
+    // There is a cookie store on reqwest, but to set the default x-xsrf-token header I need to
+    // create a new client...
+    headers.insert(reqwest::header::HeaderName::from_static("cookie"),
+                   reqwest::header::HeaderValue::from_str(&format!("Session={};", auth_info.session))?);
+
+    let authed_client = reqwest::Client::builder()
+        // Unknown CA, and I'm not sure all devices use the same...
+        .danger_accept_invalid_certs(true)
+        .default_headers(headers)
+        .build()?;
+
+    let raw_data = fetch_api(&authed_client, "network/1")?;
+    let data:Value = serde_json::from_str(&raw_data)?;
+    debug!("Got network response: {:#?}", data);
+
+    let rx = data["bandwidth"]["minutesRx"][0].as_u64().unwrap() * 8;
+    let tx = data["bandwidth"]["minutesTx"][0].as_u64().unwrap() * 8;
+    let errors = data["rxErrors"].as_u64().unwrap() * 8;
+    let dropped = data["rxDropped"].as_u64().unwrap() * 8;
+    println!("Data: rx = {}, tx = {}, errors = {}, dropped = {}", rx, tx, errors, dropped);
+
+    // TODO: there's also natEntriesUsed from /api/settings/system, which might be interesting to pull
+
+    let mut influx_map = HashMap::new();
+    influx_map.insert("net_tx", tx);
+    influx_map.insert("net_rx", rx);
+    influx_map.insert("net_rx_errors", errors);
+    influx_map.insert("net_rx_dropped", dropped);
+    let influx_data = influx_map.iter().fold(String::new(), |mut acc, (key, val)| {
+        acc.push_str(&format!("{},host=myfiosgateway.com value={}i\n", key, val));
+        acc
+    });
+    debug!("Influx data:\n{}", influx_data);
+    save_data(&client, influx_db, influx_data)?;
+
+    fetch_api(&authed_client, "logout")?;
+
+    Ok(())
+}
+
+fn get_login_info(client: &reqwest::Client) -> Result<LoginResponse, FetchError>
+{
+    let body = fetch_api(client, "login")?;
+    let info = serde_json::from_str(&body)?;
+    Ok(info)
+}
+
+fn fetch_api(client: &reqwest::Client, api: &str) -> Result<String, FetchError> {
+    let uri = reqwest::Url::parse(&format!("{}{}", BASE_URI, api))?;
+    debug!("Fetching: {}", uri);
+    let mut response = client.get(uri).send()?;
+    let body = response.text()?;
+    Ok(body)
+}
+
+fn do_login(client: &reqwest::Client, password: &str, password_salt: &str) -> Result<AuthInfo, FetchError> {
+    let mut info = AuthInfo::default();
+
+    let mut hasher = Sha512::new();
+    hasher.input(password);
+    hasher.input(password_salt);
+    let hash = hasher.result();
+    let json = format!("{{\"password\":\"{:x}\"}}", hash);
+
+    let uri = reqwest::Url::parse(&format!("{}login", BASE_URI))?;
+    let response = client.post(uri)
+        .header(reqwest::header::CONTENT_TYPE, "application/json;charset=UTF-8")
+        .body(json)
+        .send()?;
+
+    if response.status().is_success() {
+        for cookie in response.cookies() {
+            match cookie.name() {
+                "XSRF-TOKEN" => info.token = cookie.value().to_string(),
+                "Session" => info.session = cookie.value().parse().unwrap(),
+                _ => () ,
             }
-        });
+        }
+    } else {
+        bail!("Could not login: {}", response.status());
+    }
 
-    rt::run(fut);
+    Ok(info)
 }
 
-fn fetch_json(uri: hyper::Uri) -> impl Future<Item=LoginResponse, Error=FetchError> {
-    let mut tls_builder = native_tls::TlsConnector::builder();
-    tls_builder.danger_accept_invalid_certs(true);
-    let tls = tls_builder.build().unwrap();
+fn save_data(client: &reqwest::Client, influx_uri:&str, data: String) -> Result<(), FetchError> {
+    debug!("Saving data to InfluxDB: {}", influx_uri);
+    let response = client.post(influx_uri)
+        .header(reqwest::header::CONTENT_TYPE, "application/json;charset=UTF-8")
+        .body(data)
+        .send()?;
 
-    let mut http = HttpConnector::new(4);
-    http.enforce_http(false);
-
-    let https = HttpsConnector::from((http, tls));
-
-    let client = Client::builder().build::<_, hyper::Body>(https);
-
-    client
-        .get(uri)
-        .and_then(|res| {
-            res.into_body().concat2()
-        })
-        .from_err::<FetchError>()
-        .and_then(|body| {
-            let response = serde_json::from_slice(&body)?;
-
-            Ok(response)
-        })
-        .from_err()
+    if response.status() != reqwest::StatusCode::NO_CONTENT {
+        bail!("Unexpected status from InfluxDB: {}", response.status());
+    }
+    Ok(())
 }
 
+#[derive(Debug)]
 enum FetchError {
-    Http(hyper::Error),
+    Http(reqwest::Error),
+    Url(reqwest::UrlError),
     Json(serde_json::Error),
+    Simple(simple_error::SimpleError),
 }
 
-impl From<hyper::Error> for FetchError {
-    fn from(err: hyper::Error) -> FetchError {
+impl From<reqwest::Error> for FetchError {
+    fn from(err: reqwest::Error) -> FetchError {
         FetchError::Http(err)
+    }
+}
+
+impl From<reqwest::UrlError> for FetchError {
+    fn from(err: reqwest::UrlError) -> FetchError {
+        FetchError::Url(err)
     }
 }
 
 impl From<serde_json::Error> for FetchError {
     fn from(err: serde_json::Error) -> FetchError {
         FetchError::Json(err)
+    }
+}
+
+impl From<simple_error::SimpleError> for FetchError {
+    fn from(err: simple_error::SimpleError) -> FetchError {
+        FetchError::Simple(err)
+    }
+}
+
+impl std::fmt::Display for FetchError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "fetch error")
+    }
+}
+
+impl std::error::Error for FetchError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        None
     }
 }
